7f4cd4a56628d139ea5b80133a7cda918555f049
shonoiri <irina-shonova@ya.ru>
2015-07-20 17:11:05 +0300

    Update Battle.java

--- src/Battle.java
+++ src/Battle.java
@@ -1,3 +1,33 @@
-public class Battle{
-	
+
+public class Battle {
+
+	public static void main(String[] args) {
+
+		Robot r = new Robot();
+
+	//	Human h = new Human();
+
+		Robot r1 = new Robot();
+
+		GUI.getShips(r, r.field);
+		GUI.showMap(r.field);
+
+		GUI.getShips(r1, r1.field);
+		GUI.showMap(r1.field);
+		
+		//GUI.showMap(h.map);
+
+		while (!r.isLooser() && !r1.isLooser()) {
+			r1.shoot(r.field);
+			r.shoot(r1.field);
+			}
+			GUI.showMap(r.field);
+			GUI.showMap(r1.field);
+
+		//}
+		if (r.isLooser()){System.out.println(", u win, my sun, u win  ..... " + r.field.ships.size() + r1.field.ships.size());
+		}else{
+			System.out.println(", ur looser, my friend ..... " + r.field.ships.size() + r1.field.ships.size());
+		}
+	}
 }
